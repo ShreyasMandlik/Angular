f5ec94c755161902b6aa6ba4ad49443c06b40aaf
Shreyas <mandlikshreyas01@gmail.com>
2023-01-30 11:24:54 +0530

    ENH:created Add Pokemon Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Incubyte';
+  pokemonform:FormGroup;
+  constructor( private fb:FormBuilder){
+    this.pokemonform=fb.group({});
+  }
+  ngOnInit(): void {
+    this.pokemonform = this.fb.group({
+      name: this.fb.control('Abc'),
+      speciality: this.fb.control('pi'),
+      imageUrl: this.fb.control('hhtp'),
+    });
+    
+
+  }
   
 }
+
+
