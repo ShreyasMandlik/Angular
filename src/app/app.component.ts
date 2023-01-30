3e713e72a8aad9205d14313df0b6c381cd18093b
Shreyas <mandlikshreyas01@gmail.com>
2023-01-30 13:09:31 +0530

    Render the information on UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { PokemonModel } from './model/pokemon.model';
+import { PokemonService } from './services/pokemon.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Incubyte';
-  pokemonform:FormGroup;
-  constructor( private fb:FormBuilder){
-    this.pokemonform=fb.group({});
+
+  allPokemon: PokemonModel[];
+  pokemons: PokemonModel[] = [];
+  pokemonform: FormGroup;
+
+  // PokemonServices :PokemonService;
+
+  constructor(
+    private fb: FormBuilder,
+    private pokemonServices: PokemonService
+  ) {
+    this.pokemonform = fb.group({});
+    this.allPokemon = [];
   }
+
   ngOnInit(): void {
     this.pokemonform = this.fb.group({
       name: this.fb.control('Abc'),
       speciality: this.fb.control('pi'),
       imageUrl: this.fb.control('hhtp'),
     });
-    
-
+    this.pokemonServices.getPokemons().subscribe((response: any) => {
+      this.pokemons = response;
+      console.log(this.pokemons[0]);
+    });
   }
-  
 }
-
-
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PokemonModel } from './../model/pokemon.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  baseUrl = 'http://localhost:3000/Pokemon';
+
+  constructor(private http: HttpClient) {}
+  public getPokemons() {
+    return this.http.get<PokemonModel[]>(this.baseUrl);
+  }
+}
